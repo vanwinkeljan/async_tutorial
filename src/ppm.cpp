bad0640ec1c00c8f4caabd5c8343877fdf741539
vanwinkeljan <vanwinkeljan@gmail.com>
2014-03-29 17:51:28 +0100

    Added tracepoints to ppm

--- src/ppm.cpp
+++ src/ppm.cpp
@@ -6,6 +6,11 @@
 
 #include "ppm.h"
 
+// lttng-ust trace points
+#define TRACEPOINT_DEFINE
+#define TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#include "tp_ppm.h"
+
 //init with default values
 void ppm::init() {
     width = 0;
@@ -16,12 +21,16 @@ void ppm::init() {
 //create a PPM object
 ppm::ppm() {
     init();
+    tracepoint(com_vanwinkeljan_lttng_test_ppm, create,width,height,"");
 }
 
 //create a PPM object and fill it with data stored in fname 
 ppm::ppm(const std::string &fname) {
     init();
     read(fname);
+    std::stringstream tmpStrStream;
+    tmpStrStream << "Created PPM class and read data from file " << fname;
+    tracepoint(com_vanwinkeljan_lttng_test_ppm, create, width,height,fname.c_str());
 }
 
 //create an "epmty" PPM image with a given width and height;the R,G,B arrays are filled with zeros
@@ -39,6 +48,8 @@ ppm::ppm(const unsigned int _width, const unsigned int _height) {
     r.resize(size);
     g.resize(size);
     b.resize(size);
+
+    tracepoint(com_vanwinkeljan_lttng_test_ppm, create, width,height,"");
 }
 
 //read the PPM image from fname
@@ -48,7 +59,11 @@ void ppm::read(const std::string &fname) {
         std::string line;
         std::getline(inp, line);
         if (line != "P6") {
-            std::cout << "Error. Unrecognized file format." << std::endl;
+        	std::stringstream msg;
+        	msg << "Unrecognized file format (file: " << fname << ")";
+        	tracepoint(com_vanwinkeljan_lttng_test_ppm,
+        			error_message,
+        			msg.str().c_str());
             return;
         }
         std::getline(inp, line);
@@ -63,8 +78,12 @@ void ppm::read(const std::string &fname) {
             nr_lines = height;
             nr_columns = width;
         } catch (std::exception &e) {
-            std::cout << "Header file format error. " << e.what() << std::endl;
-            return;
+        	std::stringstream msg;
+        	msg << "Header file format error (file: " << fname << ")" << e.what();
+        	tracepoint(com_vanwinkeljan_lttng_test_ppm,
+        			error_message,
+        			msg.str().c_str());
+        	return;
         }
 
         std::getline(inp, line);
@@ -72,7 +91,11 @@ void ppm::read(const std::string &fname) {
         try {
             max_val >> max_col_val;
         } catch (std::exception &e) {
-            std::cout << "Header file format error. " << e.what() << std::endl;
+        	std::stringstream msg;
+        	msg << "Header file format error (file: " << fname << ")" << e.what();
+        	tracepoint(com_vanwinkeljan_lttng_test_ppm,
+        			error_message,
+        			msg.str().c_str());
             return;
         }
 
@@ -92,9 +115,14 @@ void ppm::read(const std::string &fname) {
             b[i] = (unsigned char) aux;
         }
     } else {
-        std::cout << "Error. Unable to open " << fname << std::endl;
+    	std::stringstream msg;
+    	msg << "Error. Unable to open " << fname;
+    	tracepoint(com_vanwinkeljan_lttng_test_ppm,
+    			error_message,
+    			msg.str().c_str());
     }
     inp.close();
+    tracepoint(com_vanwinkeljan_lttng_test_ppm, read,fname.c_str());
 }
 
 //write the PPM image in fname
@@ -116,7 +144,12 @@ void ppm::write(const std::string &fname) {
         }
         inp.write(&buff[0], 3*size);
     } else {
-        std::cout << "Error. Unable to open " << fname << std::endl;
+    	std::stringstream msg;
+    	msg << "Error. Unable to open " << fname;
+    	tracepoint(com_vanwinkeljan_lttng_test_ppm,
+    			error_message,
+    			msg.str().c_str());
     }
     inp.close();
+    tracepoint(com_vanwinkeljan_lttng_test_ppm, write,fname.c_str());
 }
